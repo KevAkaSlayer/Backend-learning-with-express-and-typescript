e2d78f484952091a3422f952cf38aa97934de102
Amin Al Islam <pukman41@gmail.com>
2025-08-13 18:27:12 +0600

    academic faculty interface, model , validation, service has been created

---
+++ First-project/src/app/modules/academicFaculty/academicFaculty.validation.ts
@@ -0,0 +1,10 @@
+import {z} from "zod"
+
+const academicFacultyValidationSchema = z.object({
+    name : z.string()
+})
+
+
+export const AcademicFacultyValidation = {
+    academicFacultyValidationSchema,
+}
