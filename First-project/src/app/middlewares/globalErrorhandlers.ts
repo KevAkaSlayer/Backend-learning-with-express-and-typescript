f5ef592c9c08adaaaa0338a6014afd2e1e0445b6
Amin Al Islam <pukman41@gmail.com>
2025-08-17 15:55:56 +0600

    error, AppError, unhandledRejection,UncaughtException added

--- First-project/src/app/middlewares/globalErrorhandlers.ts
+++ First-project/src/app/middlewares/globalErrorhandlers.ts
@@ -4,10 +4,13 @@ import { TErrorSources } from '../interface/error';
 import config from '../config';
 import handleZodError from '../errors/handleZodError';
 import handleValidationError from '../errors/handleValidationError';
+import handleCastError from '../errors/handleCastError';
+import handleDuplicateError from '../errors/handleDuplicateError';
+import AppError from '../errors/AppError';
 
 const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
-  let statusCode = err.status || 500; // Use a valid HTTP status code
-  let message = err.message || 'Something went wrong!';
+  let statusCode = 500; // Use a valid HTTP status code
+  let message = 'Something went wrong!';
 
 
   let errorSources: TErrorSources = [{
@@ -27,6 +30,33 @@ const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
     statusCode = simplifiedError?.statusCode;
     message = simplifiedError?.message;
     errorSources = simplifiedError?.errorSources;
+  } else if (err?.name === 'CastError') {
+    const simplifiedError = handleCastError(err);
+    statusCode = simplifiedError?.statusCode;
+    message = simplifiedError?.message;
+    errorSources = simplifiedError?.errorSources;
+  } else if (err?.code === 11000) {
+    const simplifiedError = handleDuplicateError(err);
+    statusCode = simplifiedError?.statusCode;
+    message = simplifiedError?.message;
+    errorSources = simplifiedError?.errorSources;
+  } else if (err instanceof AppError) {
+    statusCode = err?.statusCode;
+    message = err?.message;
+    errorSources = [
+      {
+        path: "",
+        message: err?.message,
+      }
+    ]
+  } else if (err instanceof Error) {
+    message = err?.message;
+    errorSources = [
+      {
+        path: "",
+        message: err?.message,
+      }
+    ]
   }
 
   res.status(statusCode).json({
